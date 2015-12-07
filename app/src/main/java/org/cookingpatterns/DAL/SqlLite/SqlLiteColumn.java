e66c13b66c5d13a7801fa263cf3bd7ac8dd28844
Andreas Gladik <andreas.gladik@gmail.com>
2015-12-07 21:13:14 +0100

    insert ingredients if not in the db on add or update recipe

--- app/src/main/java/org/cookingpatterns/DAL/SqlLite/SqlLiteColumn.java
+++ app/src/main/java/org/cookingpatterns/DAL/SqlLite/SqlLiteColumn.java
@@ -4,8 +4,8 @@ package org.cookingpatterns.DAL.SqlLite;
  * Created by Andreas on 20.11.2015.
  */
 public class SqlLiteColumn {
-    public String Name;
-    public SqlLiteDataTypes Type;
+    public final String Name;
+    public final SqlLiteDataTypes Type;
 
     public SqlLiteColumn(String name, SqlLiteDataTypes type) {
         Name = name;
